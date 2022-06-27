a7f4a70109e96d30554e5f47003eb02f153821ef
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-06-27 10:27:01 +0300

    Test navigating from posts to comments

---
+++ src/App.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
+
+type WrapperProps = {
+  children: ReactNode;
+};
+
+const wrapper = ({ children }: WrapperProps) => {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={client}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </QueryClientProvider>
+  );
+};
+
+describe("App", () => {
+  test("shows comments when number of comments is clicked", async () => {
+    render(<App />, { wrapper });
+    const user = userEvent.setup();
+
+    expect(await screen.findByText("Inflation")).toBeInTheDocument();
+    await user.click(screen.getByText("2"));
+
+    const comment = await screen.findByText(
+      "Markets can remain irrational longer than you can remain solvent."
+    );
+    expect(comment).toBeInTheDocument();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Comments } from "./components/comments";
 import { Header } from "./components/header";
 import { PostList } from "./components/postlist";
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <div className="m-5">
-          <Header />
-          <Routes>
-            <Route index element={<PostList />} />
-            <Route path=":storyId" element={<Comments />} />
-            <Route path="*" element={<h1>Not found</h1>} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <div className="m-5">
+      <Header />
+      <Routes>
+        <Route index element={<PostList />} />
+        <Route path=":storyId" element={<Comments />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
+    </div>
   );
 }
 
