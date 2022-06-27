24542d6f56174709a8147f761d24664448b8a038
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-06-27 10:44:06 +0300

    Fix wrong comments flashing when clicking on a new post

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,9 @@ import { useQuery } from "react-query";
 import { getPosts, getPostWithComments } from "../apis/hn";
 
 export function usePostWithComments(objectId: string) {
-  return useQuery("postWithComments", () => getPostWithComments(objectId));
+  return useQuery(["postWithComments", objectId], () =>
+    getPostWithComments(objectId)
+  );
 }
 
 export function usePosts() {
