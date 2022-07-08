82387d9249fd30dc04e95dc95055592985f9896a
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-07-08 10:06:54 +0300

    Use shorthand notation for post dates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,12 +3,9 @@ export function timeSince(date: Date) {
     1,
     Math.floor((new Date().valueOf() - date.valueOf()) / 36e5)
   );
-  if (hoursSince == 1) {
-    return "1 hour ago";
-  }
   if (hoursSince < 24) {
-    return `${hoursSince} hours ago`;
+    return `${hoursSince}h`;
   }
   const daysSince = Math.floor(hoursSince / 24);
-  return `${daysSince} days ago`;
+  return `${daysSince}d`;
 }
