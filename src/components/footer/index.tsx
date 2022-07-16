74d6a8be930ec4ba608500d8c11d3708734ec737
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-07-16 10:00:58 +0300

    Add GitHub link to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,20 @@
+import { GitHubIcon } from "../icons/github";
+
 export const Footer = () => (
-  <footer className="text-center text-gray-400 dark:text-gray-200">
-    Powered by{" "}
-    <a href="https://hn.algolia.com/api" target="_blank" rel="noreferrer">
-      Algolia
+  <footer className="flex gap-2 justify-center items-center text-gray-400 dark:text-gray-200">
+    <a
+      href="https://github.com/coocos/hacker-news"
+      target="_blank"
+      rel="noreferrer"
+      className="fill-gray-700 dark:fill-gray-300"
+    >
+      <GitHubIcon />
     </a>
+    <div>
+      Powered by{" "}
+      <a href="https://hn.algolia.com/api" target="_blank" rel="noreferrer">
+        Algolia
+      </a>
+    </div>
   </footer>
 );
